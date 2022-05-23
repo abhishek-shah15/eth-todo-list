022595a160bb63e1dd295d44101cc43a7662769e
Abhishek Shah <ab.shah15@gmail.com>
2022-05-23 17:50:53 +0530

    Update SmartContract for First Task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+    // 0 => {1303,First task, true}
+    // 1 => {1293,Second task1, false}
+
+    constructor() public{
+        createTask("First Task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
